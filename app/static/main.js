5c35616ce014b0aca8d418de16c23885f79c7abd
Aaryan Thakur <princeaaryan06@gmail.com>
2025-06-15 19:38:31 +0545

    update

--- app/static/main.js
+++ app/static/main.js
@@ -53,6 +53,7 @@ userForm.addEventListener("submit", async (e) => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(userData),
+      
     });
   }
 
